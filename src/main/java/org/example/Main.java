206f3c1e807c1e5ef8efc3248ccd94051df11ff5
NishantBudia <nishantbudia@gmail.com>
2023-09-11 13:46:41 +0530

    Made api call and got data in json

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,7 +14,7 @@ import java.net.URL;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String myurl = "https://api.chucknorris.io/jokes/random";
+        String myurl = "https://api.zippopotam.us/us/33162";
 
         HttpURLConnection connection = null;
         int responseCode = 0;
